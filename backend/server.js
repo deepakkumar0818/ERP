978da78c203173056bb9919481b32235a6ea4b55
Nitin <nitin.echo@gmail.com>
2026-03-09 18:18:25 +0530

    updated frontend

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-
+const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
@@ -18,7 +18,7 @@ app.get('/', (req, res) => {
   res.send('Factory ERP Backend is Running!');
 });
 
-const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
